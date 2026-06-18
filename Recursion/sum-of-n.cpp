82ff98dfde78eb2a335b7ec83cbd9a963134f7b1
shreekrishnadevkota <devkotaroshan104@gmail.com>
2026-06-18 07:58:06 +0545

    sum of n number

---
+++ Recursion/sum-of-n.cpp
@@ -0,0 +1,38 @@
+// using the formula n(n+1)/2, we can calculate the sum of first n natural numbers as shown below:
+
+#include <iostream>
+using namespace std;
+
+int sum(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return n + sum(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    cout << "Sum of first " << n << " natural numbers is: " << sum(n) << endl;
+    return 0;
+}
+
+// without using the formula n(n+1)/2, we can also calculate the sum of first n natural numbers using a for loop as shown below:
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i, n, sum=0;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    for (i=1; i<=n; i++){
+        sum += i;
+    }
+    cout << "Sum of first " << n << " natural numbers is: " << sum << endl;
+    return 0;   
+}
+
+
